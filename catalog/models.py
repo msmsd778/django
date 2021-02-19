dc29c8a5b2302c91f83429cb73b0bcceb7a4074a
Masoud <msmsd778@gmail.com>
2021-02-19 22:05:23 +0330

    adding login, permissions & actions on views

--- catalog/models.py
+++ catalog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.urls import reverse #Used to generate URLs by reversing the URL patterns
 import uuid # Required for unique book instances
-
+from django.contrib.auth.models import User
+from datetime import date
 
 class Book(models.Model):
     """
@@ -57,9 +58,11 @@ class BookInstance(models.Model):
     )
 
     status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability')
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
         ordering = ["due_back"]
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
         """
@@ -67,6 +70,11 @@ class BookInstance(models.Model):
         """
         return '%s (%s)' % (self.id,self.book.title)
 
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
 
 class Author(models.Model):
